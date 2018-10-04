fd43080afe5d1225a82121c88ca42ec815dde421
Robert Estey <robert.estey@perfect.com>
2018-10-03 20:49:52 -0400

    putting back basics

--- bootstrap-angular-cv64/src/app/app.component.ts
+++ bootstrap-angular-cv64/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'bootstrap-angular-cv64';
+
+  @Input() count: number = 0;
+  @Output() countChange = new EventEmitter<number>();
+
+  constructor() {
+    console.log('AppComponent.constructor');
+  }
+
+  increment() {
+    this.count++;
+    this.countChange.emit(this.count);
+    console.log('increment current count: ' + this.count);
+  }
+
+  decrement() {
+    this.count--;
+    this.countChange.emit(this.count);
+    console.log('decrement current count: ' + this.count);
+  }
 }
