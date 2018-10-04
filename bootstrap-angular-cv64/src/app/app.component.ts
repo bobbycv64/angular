ee557bf0076688f67be8efd88ddba468818881ad
Robert Estey <robert.estey@perfect.com>
2018-10-03 23:11:57 -0400

    updated code

--- bootstrap-angular-cv64/src/app/app.component.ts
+++ bootstrap-angular-cv64/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,26 +6,55 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'bootstrap-angular-cv64';
+  twoWay = '';
+  items:string[] = null;
 
-  @Input() count: number = 0;
+  @Input() count: number = 3;
   @Output() countChange = new EventEmitter<number>();
+
+
 
   constructor() {
     console.log('AppComponent.constructor');
+  }
+
+  ngOnInit() {
+    this.items = ['Action', 'Another action', 'Yet Another Action'];
   }
 
   increment() {
     this.count++;
     this.countChange.emit(this.count);
     console.log('increment current count: ' + this.count);
+
+    // push = add item to array
+    this.items.push('increment ' + String(this.count));
   }
 
   decrement() {
     this.count--;
     this.countChange.emit(this.count);
     console.log('decrement current count: ' + this.count);
+
+    // pop = remove item from array
+    this.items.pop();
+  }
+
+  addItem(item) {
+    console.log('addItem: ' + item);
+    this.items.push(item);
+    return false;
+  }
+
+  deleteItem(item) {
+    console.log('deleteItem: ' + item);
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] == item) {
+        this.items.splice(i, 1);
+      }
+    }
   }
 }
