fa7b4c04179fed701b7e8ba4e999329b41c09b17
Robert Estey <robert.estey@perfect.com>
2018-11-06 22:22:59 -0500

    initial

---
+++ firebase-crud-angular-cv64/src/app/shared/customer.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private angularFireDatabase: AngularFireDatabase) { }
+
+  customerList: AngularFireList<any>;
+
+  form = new FormGroup({
+    $key: new FormControl(null),
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email),
+    mobile: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    location: new FormControl('')
+  });
+
+  getCustomers() {
+
+    // retreive all customers from the firebase database from the customers node
+    this.customerList = this.angularFireDatabase.list('customers');
+
+    // return from an observable (snapshotChanges) from customerList
+    return this.customerList.snapshotChanges();
+  }
+
+// create unique key for the firebase database
+  insertCustomer(customer) {
+    this.customerList.push({
+      fullName: customer.fullName,
+      email: customer.email,
+      mobile: customer.mobile,
+      location: customer.location
+    });
+  }
+
+  populateForm(customer) {
+    this.form.setValue(customer);
+  }
+
+  updateCustomer(customer) {
+    this.customerList.update(customer.$key,
+    {
+      fullName: customer.fullName,
+      email: customer.email,
+      mobile: customer.mobile,
+      location: customer.location
+    });
+  }
+
+  deleteCustomer($key: string) {
+    this.customerList.remove($key);
+  }
+}
---
+++ firebase-crud-angular-cv64/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBj7prssKofiXqgwPyQUXzhs9lJH9o6JT8",
+    authDomain: "fir-crudapp-fc66b.firebaseapp.com",
+    databaseURL: "https://fir-crudapp-fc66b.firebaseio.com",
+    projectId: "fir-crudapp-fc66b",
+    storageBucket: "fir-crudapp-fc66b.appspot.com",
+    messagingSenderId: "404964738792"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
